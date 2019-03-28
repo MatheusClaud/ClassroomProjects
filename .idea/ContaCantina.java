6a9303b19845f7a623ef1d994fc008dae27de421
118210245 MATHEUS MACEDO CLAUDINO <matheusmc@lcc3-079.lcc.ufcg.edu.br>
2019-03-28 08:20:05 -0300

    adding class coisa and class ContaCantina.java

---
+++ .idea/ContaCantina.java
@@ -0,0 +1,36 @@
+public class ContaCantina {
+
+    String nome;
+    int debito;
+    int quantidade = 0;
+    int gastosTotais = 0;
+
+    public ContaCantina(String nome){
+        this.nome = nome;
+    }
+
+    public void cadastraLanche(int quantidade, int valor){
+        debito += valor;
+        this.quantidade += quantidade;
+        this.gastosTotais += valor;
+    }
+
+    public void pagaConta(int valor){
+        debito += valor;
+    }
+
+    public int getFaltaPagar(){
+        return  debito;
+    }
+
+    @Override
+    public String toString(){
+        return nome + " " + quantidade + " " + gastosTotais;
+    }
+
+
+
+
+
+
+}
